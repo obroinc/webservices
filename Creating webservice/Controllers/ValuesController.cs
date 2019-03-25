6a83f85f34c2e21c742afe18b9111052697d5928
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-03-25 19:57:34 +0000

    Add project files.

---
+++ Creating webservice/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Creating_webservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Have", "Hope" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id == 1)
+            {
+                return "There is hope for the fuure";
+            }
+            else
+            {
+                return "value";
+            }
+            
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Creating webservice/Data/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creating webservice.Data 
+{
+    public class TodoContext:DbContext
+
+{
+    public TodoContext(DbContextOptions<TodoContext>options)
+        : base(options)
+    {
+
+    }
+    public DbSet<TodoItem> ToDoItems { get; set; }
+
+}
+}
